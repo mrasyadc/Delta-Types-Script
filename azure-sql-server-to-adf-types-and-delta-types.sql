b0deb77db29b07473674c29e7afe9773c085b9aa
Muhammad Rasyad Caesarardhi <56964497+mrasyadc@users.noreply.github.com>
2025-10-24 11:37:30 +0700

    Update azure-sql-server-to-adf-types-and-delta-types.sql

--- azure-sql-server-to-adf-types-and-delta-types.sql
+++ azure-sql-server-to-adf-types-and-delta-types.sql
@@ -7,15 +7,15 @@ SELECT
             WHEN data_type IN ('bigint', 'int', 'smallint', 'tinyint') THEN 'long'
             WHEN data_type IN ('bit') THEN 'boolean'
             WHEN data_type IN ('float', 'real') THEN 
-                'decimal(' || 
-                COALESCE(CAST(numeric_precision AS VARCHAR), '38') || 
-                ',' || 
-                COALESCE(CAST(numeric_scale AS VARCHAR), '18') || 
+                'decimal(' + 
+                COALESCE(CAST(numeric_precision AS VARCHAR), '38') + 
+                ',' + 
+                COALESCE(CAST(numeric_scale AS VARCHAR), '18') + 
                 ')'
             WHEN data_type IN ('decimal', 'numeric') THEN
                 CASE 
                     WHEN COALESCE(numeric_precision, 0) = 0 AND COALESCE(numeric_scale, 0) = 0 THEN 'decimal(38,18)'
-                    ELSE 'decimal(' || COALESCE(CAST(numeric_precision AS VARCHAR), '0') || ',' || COALESCE(CAST(numeric_scale AS VARCHAR), '0') || ')'
+                    ELSE 'decimal(' + COALESCE(CAST(numeric_precision AS VARCHAR), '0') + ',' + COALESCE(CAST(numeric_scale AS VARCHAR), '0') + ')'
                 END
             WHEN data_type IN ('uniqueidentifier', 'varchar', 'nvarchar', 'json', 'text', 'ntext', 'char', 'nchar') THEN 'string'
             WHEN data_type IN ('date') THEN 'date'
@@ -43,30 +43,44 @@ SELECT
             WHEN data_type IN ('binary', 'varbinary', 'image') THEN 'BinaryType()'
             -- Handle DecimalType with precision/scale
             WHEN data_type IN ('float', 'real') THEN 
-                'DecimalType(' || 
-                COALESCE(CAST(numeric_precision AS VARCHAR), '38') || 
-                ',' || 
-                COALESCE(CAST(numeric_scale AS VARCHAR), '18') || 
+                'DecimalType(' + 
+                COALESCE(CAST(numeric_precision AS VARCHAR), '38') + 
+                ',' + 
+                COALESCE(CAST(numeric_scale AS VARCHAR), '18') + 
                 ')'
             WHEN data_type IN ('decimal', 'numeric') THEN
                 CASE 
                     WHEN COALESCE(numeric_precision, 0) = 0 AND COALESCE(numeric_scale, 0) = 0 
                         THEN 'DecimalType(38,18)'
                     ELSE 
-                        'DecimalType(' || 
-                        COALESCE(CAST(numeric_precision AS VARCHAR), '0') || 
-                        ',' || 
-                        COALESCE(CAST(numeric_scale AS VARCHAR), '0') || 
+                        'DecimalType(' + 
+                        COALESCE(CAST(numeric_precision AS VARCHAR), '0') + 
+                        ',' + 
+                        COALESCE(CAST(numeric_scale AS VARCHAR), '0') + 
                         ')'
                 END
             ELSE 'StringType()'
         END,
-        ', True), \\'  -- Maintain backslash for line continuation
+        ', True), \'  -- Maintain backslash for line continuation
     ) AS struct_field
 FROM
     information_schema.COLUMNS
 WHERE
-    table_name IN ('MM_BRANDCATEGORY')
+    table_name IN ('ANALYSISJOB',
+'ANALYSISRESULT',
+'HAULINGREQUEST',
+'HAULINGREQUEST_DETAIL_PORTIONBLENDING',
+'LOADING_REQUEST_BLU',
+'LOADING_REQUEST_BLU_BARGE',
+'LOADING_REQUEST_BLU_BARGE_PRODUCT',
+'LOADING_REQUEST_LOADING_PLAN',
+'SAMPLE',
+'SAMPLEDETAILGENERAL',
+'SAMPLEDETAILLOADING',
+'SAMPLESCHEME',
+'SCHEME',
+'TUNNEL',
+'UPLOAD_SAMPLING_ROM')
 ORDER BY
     table_schema,
     table_name,
